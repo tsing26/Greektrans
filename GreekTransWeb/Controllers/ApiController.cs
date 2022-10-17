c715a5bf6caf7ad3a99ffdb239008f7905d678bb
xietao <xietao@dp2003.com>
2022-10-17 10:20:08 +0800

    增加 Swagger 支持

---
+++ GreekTransWeb/Controllers/ApiController.cs
@@ -0,0 +1,129 @@
+﻿using System.Net;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Filters;
+
+using GreekTrans;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace GreekTransWeb.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    //[Route("greektrans/[controller]/[action]")]
+    [Route("api/[action]")]
+    [Route("api/v1/[action]")]
+
+    public class ApiController : Controller
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [SwaggerRequestExample(typeof(TransliterRequest), typeof(TransliterRequestExamples))]
+        [SwaggerResponseExample((int)HttpStatusCode.OK, typeof(TransliterResponseExamples))]
+        [HttpPost]
+        public TransliterResponse Transliter([FromBody] TransliterRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Greek))
+                return new TransliterResponse
+                {
+                    ErrorInfo = $"{nameof(request.Greek)} parameter should not be empty"
+                };
+            try
+            {
+                StringBuilder details = new StringBuilder();
+                var result = GreekTransliter.TransliterString(request.Greek,
+                    request.Ancient,
+                    details);
+                return new TransliterResponse
+                {
+                    Translitered = result,
+                    Details = details?.ToString(),
+                };
+            }
+            catch(Exception ex)
+            {
+                return new TransliterResponse
+                {
+                    ErrorInfo = $"exception: {ex.Message}",
+                };
+            }
+        }
+
+        [HttpGet]
+        public TransliterResponse Transliter(string greek, bool ancient = false)
+        {
+            if (string.IsNullOrEmpty(greek))
+                return new TransliterResponse
+                {
+                    ErrorInfo = $"{nameof(greek)} parameter should not be empty"
+                };
+            try
+            {
+                StringBuilder details = new StringBuilder();
+                var result = GreekTransliter.TransliterString(greek,
+                    ancient,
+                    details);
+                return new TransliterResponse
+                {
+                    Translitered = result,
+                    Details = details?.ToString(),
+                };
+            }
+            catch (Exception ex)
+            {
+                return new TransliterResponse
+                {
+                    ErrorInfo = $"exception: {ex.Message}",
+                };
+            }
+        }
+
+
+        public class TransliterRequest
+        {
+            public string? Greek { get; set; }
+            public bool Ancient { get; set; }
+        }
+
+        public class TransliterResponse
+        {
+            public string? Translitered { get; set; }
+
+            public string? Details { get; set; }
+
+            public string? ErrorInfo { get; set; }
+        }
+
+        public class TransliterResponseExamples : IExamplesProvider<TransliterResponse>
+        {
+            public TransliterResponse GetExamples()
+            {
+                return new TransliterResponse
+                {
+                    Translitered = "hagineō",
+                    Details = "ἁ->ha γ->g ῑ->i ν->n έ->e ω->ō ",
+                    ErrorInfo = null,
+                };
+            }
+        }
+
+        public class TransliterRequestExamples : IExamplesProvider<TransliterRequest>
+        {
+            public TransliterRequest GetExamples()
+            {
+                return new TransliterRequest
+                {
+                    Greek = "ἁγῑνέω",
+                    Ancient = true,
+                };
+            }
+        }
+    }
+}
--- GreekTransWeb/Program.cs
+++ GreekTransWeb/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using static GreekTransWeb.Controllers.ApiController;
+
 namespace GreekTransWeb
 {
     public class Program
@@ -7,7 +11,51 @@ namespace GreekTransWeb
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews()
+                // https://stackoverflow.com/questions/36452468/swagger-ui-web-api-documentation-present-enums-as-strings
+                .AddJsonOptions(x =>
+                {
+                    // 禁止返回的属性名使用 camel 形态
+                    // https://stackoverflow.com/questions/58476681/asp-net-core-3-0-system-text-json-camel-case-serialization
+                    x.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    x.JsonSerializerOptions.WriteIndented = true;
+                    // x.JsonSerializerOptions.Converters.Add(new Controllers.v3Controller.ByteArrayConverter());
+                });
+
+            {
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    // https://stackoverflow.com/questions/58834430/c-sharp-net-core-swagger-trying-to-use-multiple-api-versions-but-all-end-point
+                    c.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "GreekTrans API V1",
+                        Description = "GreekTrans API 希腊文罗马化",
+                        Version = "v1",
+                        License = new OpenApiLicense
+                        {
+                            Name = "Apache-2.0",
+                            Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                        },
+                        Contact = new OpenApiContact
+                        {
+                            Name = "xietao",
+                            Email = "xietao@dp2003.com",
+                            Url = new Uri("https://github.com/tsing26/Greektrans")
+                        }
+                    });
+
+                    // c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                    // version < 3.0 like this: c.OperationFilter<ExamplesOperationFilter>(); 
+                    // version 3.0 like this: c.AddSwaggerExamples(services.BuildServiceProvider());
+                    // version > 4.0 like this:
+                    c.ExampleFilters();
+                    // c.SchemaFilter<EnumSchemaFilter>();
+
+                });
+
+                builder.Services.AddSwaggerExamplesFromAssemblyOf<TransliterResponseExamples>();
+            }
 
             var app = builder.Build();
 
@@ -18,6 +66,13 @@ namespace GreekTransWeb
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", "GreekTrans API V1");
+                c.SwaggerEndpoint($"/swagger/swagger.json", "GreekTrans API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
